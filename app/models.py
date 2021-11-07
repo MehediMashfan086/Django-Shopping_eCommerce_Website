ef0336cf77e0323ae834fbd1b420e67d04943247
MehediMashfan086 <mehedihasan62086@gmail.com>
2021-11-07 15:43:56 +0600

    Saving to GitHub

--- app/models.py
+++ app/models.py
@@ -2,7 +2,6 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.core.validators import MaxValueValidator, MinValueValidator
 
-
 STATE_CHOICES = (
     ('Barisal','Barisal'),
     ('Chittagong','Chittagong'),
@@ -42,4 +41,32 @@ class Product(models.Model):
     discounted_price = models.FloatField()
     description = models.TextField()
     brand = models.CharField(max_length=100)
-    catagory = models.CharField(choices=CATAGORY_CHOICES, max_length=2)
+    catagory = models.CharField(choices=CATAGORY_CHOICES, max_length=10)
+    product_image = models.ImageField(upload_to = 'productimg')
+
+def __str__(self):
+    return str(self.id)
+
+class Cart(models.Model):
+    user = models.ForeignKey(User, on_delete = models.CASCADE)
+    product = models.ForeignKey(Product, on_delete = models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+
+def __str__(self):
+    return str(self.id)
+
+STATUS_CHOICES = (
+    ('Accepted','Accepted'),
+    ('Packed','Packed'),
+    ('On The way','On The way'),
+    ('Delivered','Delivered'),
+    ('Canccel','Cancel'),
+)
+
+class OrderPlaced(models.Model):
+    user = models.ForeignKey(User, on_delete = models.CASCADE)
+    Customer = models.ForeignKey(Customer, on_delete = models.CASCADE)
+    product = models.ForeignKey(Product, on_delete = models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+    ordered_date = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(choices=STATUS_CHOICES, max_length=50, default='Pending')
