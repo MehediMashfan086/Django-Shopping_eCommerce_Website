fd6122cb047bd04c424924d04d05a29a1f450a0d
MehediMashfan086 <mehedihasan62086@gmail.com>
2021-11-12 23:05:07 +0600

    Saving to GitHub

--- Shopping_eCommerce_Website/urls.py
+++ Shopping_eCommerce_Website/urls.py
@@ -6,9 +6,8 @@ from django.conf import settings
 from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('', views.home),
     path('', views.ProductView.as_view(), name = 'home'),
-    path('product-detail/<int:pk>', views.product_detail, name='product-detail'),
+    path('product-detail/<int:pk>', views.ProductDetailView.as_view(), name='product-detail'),
     path('cart/', views.add_to_cart, name='add-to-cart'),
     path('buy/', views.buy_now, name='buy-now'),
     path('profile/', views.profile, name='profile'),
