da759de792de61af9f06acdf572cebe7ef5bff2f
MehediMashfan086 <mehedihasan62086@gmail.com>
2021-11-13 14:41:02 +0600

    Saving to GitHub

--- app/forms.py
+++ app/forms.py
@@ -1,9 +1,9 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from django.forms import widgets
 
 class CustomerRegistrationForm(UserCreationForm):
+    username = forms.CharField(label="Username", widget=forms.TextInput(attrs={'class':'form-control'}))
     password1 = forms.CharField(label="Password", widget=forms.PasswordInput(attrs={'class':'form-control'}))
     password2 = forms.CharField(label="Confirm Password (again)", widget=forms.PasswordInput(attrs={'class':'form-control'}))
     email = forms.CharField(required = True, widget=forms.EmailInput(attrs={'class':'form-control'}))
@@ -12,4 +12,3 @@ class Meta:
     model = User
     fields = ['username', 'email', 'password1', 'password2']
     labels = {'email':'Email'}
-    widgets = {'username': forms.TextInput(attrs={'class':'form-control'})}
