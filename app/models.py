e87ba5f62d2ca34ec3029ce3bb687bfdfa2cd882
MehediMashfan086 <mehedihasan62086@gmail.com>
2021-11-16 12:45:11 +0600

    Saving to GitHub

--- app/models.py
+++ app/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django.core.validators import MaxValueValidator, MinValueValidator
+from django.core.validators import MaxValueValidator, MinValueValidator, RegexValidator
 
 STATE_CHOICES = (
     ('Barisal','Barisal'),
