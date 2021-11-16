8c6965e431c0e8eef233aa9c5dd7d403fa759100
MehediMashfan086 <mehedihasan62086@gmail.com>
2021-11-16 11:35:22 +0600

    Saving to GitHub

--- app/forms.py
+++ app/forms.py
@@ -7,10 +7,10 @@ from django.contrib.auth import password_validation
 from app.models import Customer
 
 class CustomerRegistrationForm(UserCreationForm):
-    username = forms.CharField(label='Password', widget=forms.TextInput(attrs={'class':'form-control'}))
+    username = forms.CharField(label='Username', widget=forms.TextInput(attrs={'class':'form-control'}))
+    email = forms.CharField(required = True, widget=forms.EmailInput(attrs={'class':'form-control'}))
     password1 = forms.CharField(label='Password', widget=forms.PasswordInput(attrs={'class':'form-control'}))
     password2 = forms.CharField(label='Confirm Password (again)', widget=forms.PasswordInput(attrs={'class':'form-control'}))
-    email = forms.CharField(required = True, widget=forms.EmailInput(attrs={'class':'form-control'}))
 
 class Meta:
     model = User
--- app/views.py
+++ app/views.py
@@ -33,6 +33,22 @@ class ProfileView(View):
     def get(self, request):
         form = CustomerProfileForm()
         return render(request, 'app/profile.html', {'form':form, 'active': 'btn-primary'})
+    
+    def post(self, request):
+        form = CustomerProfileForm(request.POST)
+        if form.is_valid():
+            usr = request.user
+            name = form.cleaned_data['name']
+            locality = form.cleaned_data['locality']
+            city = form.cleaned_data['city']
+            zipcode = form.cleaned_data['zipcode']
+            state = form.cleaned_data['state']
+            reg = Customer(user = usr, name = name, locality = locality, city = city,
+                  zipcode = zipcode, state = state)
+            reg.save()
+            messages.success(request, 'Congratulations !! Your Profile Updates Successfully.')
+        return render(request, 'app/profile.html', {'form':form, 'active': 'btn-primary'})    
+        
 
 def address(request):
  return render(request, 'app/address.html')
