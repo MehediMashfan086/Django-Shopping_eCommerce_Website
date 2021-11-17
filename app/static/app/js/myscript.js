4ba35fdc482c7eb48674f3df0e5965289b7d226a
MehediMashfan086 <mehedihasan62086@gmail.com>
2021-11-17 22:42:52 +0600

    Saving to GitHub

--- app/static/app/js/myscript.js
+++ app/static/app/js/myscript.js
@@ -21,3 +21,8 @@ $('#slider1, #slider2, #slider3, #slider4').owlCarousel({
         }
     }
 })
+
+$('.plus-cart').click(function(){
+    var id = $(this).attr("pid").toString();
+    console.log(id)
+})
