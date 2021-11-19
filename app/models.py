1874e270472bcb81859b786369c079bb18e97391
MehediMashfan086 <mehedihasan62086@gmail.com>
2021-11-19 23:28:40 +0600

    Saving to GitHub

--- app/models.py
+++ app/models.py
@@ -78,3 +78,7 @@ class OrderPlaced(models.Model):
     quantity = models.PositiveIntegerField(default=1)
     ordered_date = models.DateTimeField(auto_now_add=True)
     status = models.CharField(choices=STATUS_CHOICES, max_length=50, default='Pending')
+
+    @property
+    def total_cost(self):
+        return self.quantity * self.product.discounted_price
